c9553bda766f4576fda2743491ac30b735067b13
Scrat Knapp <hatdescript@gmail.com>
2024-03-11 01:38:58 -0500

    Forgot this

---
+++ ixhl2rp/schema/items/blueprints/sh_shiblueprint.lua
@@ -0,0 +1,8 @@
+ITEM.name = "Shi Composite Armor Schematics"
+ITEM.model = "models/mosi/fallout4/props/junk/blueprint.mdl"
+ITEM.description = "A schematic produced by Shi materials science efforts for making an improved combat armor. While a full suit requires full production capability, this recipe should enable you to make some of its plates to add to your own kit."
+ITEM.category = "Blueprints"
+ITEM.flag = "3"
+ITEM.price = 125
+ITEM.width = 1
+ITEM.height = 1
