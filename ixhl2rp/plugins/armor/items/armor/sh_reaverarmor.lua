7aff18a08190f2cceb144de05146134e70c81b67
Xenokkah <56182717+Xenokkah@users.noreply.github.com>
2024-04-04 23:42:03 -0400

    Add reaver armor

---
+++ ixhl2rp/plugins/armor/items/armor/sh_reaverarmor.lua
@@ -0,0 +1,24 @@
+ITEM.name = "Reaver Armor"
+ITEM.model = "models/thespireroleplay/items/clothes/group012.mdl"
+ITEM.replacements = {
+	{"group004", "group012"}
+}
+ITEM.description = "A suit of intact combat armor pulled from the body of a Feral Reaver."
+ITEM.longdesc = "Once the standard issue armor for US Army troops, this suit offers a fine balance of mobility and protection for the people and organizations that can actually afford it. This one is in green and brown camo.\nPulled from the body of a Feral Reaver and restored, the armor has lost some of its protective values and suffers radiation vulnerability."
+ITEM.width = 2
+ITEM.height = 1
+ITEM.price = 230000
+ITEM.flag = "A"
+ITEM.isArmor = true
+ITEM.isBodyArmor = true
+ITEM.noBusiness = true
+ITEM.dT = 14
+ITEM.eT = 6
+ITEM.dR = 5
+ITEM.radResist = -20
+ITEM.newSkin = 0
+ITEM.weightClass = 3
+ITEM.bodyGroups = {
+["outfit"] = 0,
+}
+ITEM.bodygroupsub = {0}
