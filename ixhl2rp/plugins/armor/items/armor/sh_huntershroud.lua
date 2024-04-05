70d644dcc8efe4526bc1d95ab7afbdee1acb777f
Xenokkah <56182717+Xenokkah@users.noreply.github.com>
2024-04-04 23:44:01 -0400

    Add unique wayfarer

---
+++ ixhl2rp/plugins/armor/items/armor/sh_huntershroud.lua
@@ -0,0 +1,27 @@
+ITEM.name = "Hunter's Shroud"
+ITEM.model = "models/thespireroleplay/items/clothes/group015.mdl"
+ITEM.replacements = {
+	{"group004", "group015"}
+}
+ITEM.description = "A modified set of wayfarer's robes, smelling faintly of tannin."
+ITEM.longdesc = "A baggy set of armor that's a cut above your standard leather armor, made with care for those who spend long days traveling through the sun potentially facing hostile threats. Often fashioned by tribals.\nThis particular set of robes (and the armour that goes with them) have been hardened for use in hunting wasteland creatures.\n+5 modifier when sneaking"
+ITEM.width = 2
+ITEM.height = 1
+ITEM.price = 230000
+ITEM.flag = "A"
+ITEM.isArmor = true
+ITEM.isBodyArmor = true
+ITEM.noBusiness = true
+ITEM.weight = 2
+ITEM.weight = 2
+ITEM.dT = 14
+ITEM.eT = 6
+ITEM.skillBoosts = {
+	["survival"] = 5
+}
+ITEM.newSkin = 0
+ITEM.weightClass = 1
+ITEM.bodyGroups = {
+
+}
+ITEM.bodygroupsub = {0}
