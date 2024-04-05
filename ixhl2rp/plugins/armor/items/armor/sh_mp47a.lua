14eb151f17c421f2c092e65937fa0406f1b7df82
Xenokkah <56182717+Xenokkah@users.noreply.github.com>
2024-04-04 23:43:30 -0400

    Add medic PA

---
+++ ixhl2rp/plugins/armor/items/armor/sh_mp47a.lua
@@ -0,0 +1,30 @@
+ITEM.name = "MP-47/A Medic Power Armor"
+ITEM.model = "models/thespireroleplay/items/clothes/group056.mdl"
+ITEM.replacements = {
+	{"group004", "group056"}
+}
+ITEM.description = "A limited production model of T-45 Power Armor, equipped with an onboard medical programming package."
+ITEM.longdesc = "The most common variant of the first power armor model issued to US Troops during the war against China, the T-45d offers stronger, heavier servos that more advanced units lack, but they are considerably more sluggish even when used by a trained operator.\nThe suit is capable of injecting Med-X, Stimpaks, anticoagulants, and other medical supplies that are loaded into the suit's reserves. Prohibitive production costs and a lack of time meant the armor never saw widespread production. This suit's self-destruct capabilities have been removed.\n+2 STR, -2 AGI"
+ITEM.width = 2
+ITEM.height = 1
+ITEM.price = 230000
+ITEM.flag = "A"
+ITEM.isArmor = true
+ITEM.isBodyArmor = true
+ITEM.noBusiness = true
+ITEM.weight = 2
+ITEM.dT = 20
+ITEM.eT = 16
+ITEM.dR = 26
+ITEM.attribBoosts = {
+	["strength"] = 2,
+	["agility"] = -2
+}
+
+ITEM.newSkin = 0
+ITEM.weightClass = 3
+ITEM.bodyGroups = {
+	["headgear"] = 5,
+	["outfit"] = 0,
+}
+ITEM.bodygroupsub = {0}
