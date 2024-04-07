130e4dca86e9e751a2873490a3696ec35e9c0c18
Xenokkah <56182717+Xenokkah@users.noreply.github.com>
2024-04-07 16:40:07 -0400

    Create sh_cooking_brahminwellington.lua

---
+++ ixhl2rp/plugins/ixcraft/recipes/sh_cooking_brahminwellington.lua
@@ -0,0 +1,29 @@
+RECIPE.name = "Brahmin Wellington"
+RECIPE.description = "Prepare a Brahmin Wellington. Time consuming and difficult to produce, few meals can match this wasteland delight."
+RECIPE.model = "models/mosi/fallout4/props/food/deathclawsouffle.mdl"
+RECIPE.category = "Food"
+RECIPE.requirements = {
+	["brahminmeatraw"] = 1,
+	["antegg"] = 1,
+	["blamcomacncheese"] = 1,
+}
+
+RECIPE.results = {
+	["brahminwellington"] = 1
+}
+
+RECIPE:PostHook("OnCanSee", function(recipeTable, client)
+	if (client:GetCharacter():GetSkill("survival", 0) < 40) then 
+		return false
+	end 
+
+
+RECIPE:PostHook("OnCanSee", function(recipeTable, client)
+	for _, v in pairs(ents.FindByClass("ix_station_cookingfire")) do
+		if (client:GetPos():DistToSqr(v:GetPos()) < 200 * 40) then
+			return true
+		end
+	end
+
+	return false
+end)
