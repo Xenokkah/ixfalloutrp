f39c8c9e4214211e411a032c1230bf4acb158a39
Xenokkah <56182717+Xenokkah@users.noreply.github.com>
2024-04-07 13:32:05 -0400

    Create sh_medicpump.lua

---
+++ ixhl2rp/plugins/armor/items/upgrades/sh_medicpump.lua
@@ -0,0 +1,12 @@
+ITEM.name = "Medic Pump"
+ITEM.description = "A complex integrated medical and vital monitoring system that automatically injects stimpaks when the user is wounded.\nStimpaks no longer cost AP to use."
+ITEM.category = "Upgrades"
+ITEM.model = "models/weapons/w_suitcase_passenger.mdl"
+ITEM.width = 2
+ITEM.height = 2
+ITEM.price = 12000
+ITEM.slot = 1
+ITEM.flag = "1"
+ITEM.isUpgrade = true
+ITEM.weight = 1.1
+ITEM.isArmorUpg = true
