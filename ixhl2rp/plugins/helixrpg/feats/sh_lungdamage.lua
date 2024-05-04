ec05de9e7c427f924b3e4a691b8b01f62a4e1574
Xenokkah <56182717+Xenokkah@users.noreply.github.com>
2024-05-04 16:04:04 -0400

    Equipment changes, add new long term injuries
    Check this out yo

---
+++ ixhl2rp/plugins/helixrpg/feats/sh_lungdamage.lua
@@ -0,0 +1,4 @@
+FEAT.name = "Lung Damage"
+FEAT.description = "Severe damage has been inflicted to one or both of your lungs.\nYou are unable to Grapple or use Power Attacks."
+FEAT.icon = "fonvui/hud/icons/message/vaultboy_pain.png"
+FEAT.noStartSelection = true
---
+++ ixhl2rp/schema/items/medical/sh_surgicaltools.lua
@@ -0,0 +1,52 @@
+ITEM.name = "Case of Surgical Tools"
+ITEM.description = "A set of surgical tools in a marked metal box."
+ITEM.longdesc = "A set containing a few sets of surgical tools, still wrapped in sterile packaging.\nUsed to treat long term injuries by experienced doctors, curing or alleviating them."
+ITEM.model = "models/mccarran/medbay/firstaidkit01.mdl"
+ITEM.width = 1
+ITEM.height = 1
+ITEM.category = "Medical"
+ITEM.price = "4000"
+ITEM.flag = "1"
+ITEM.quantity = 2
+ITEM.weight = 0.05
+ITEM.duration = 350
+
+ITEM.functions.use = {
+	name = "Use",
+	icon = "icon16/key.png",
+	OnRun = function(item)
+		local quantity = item:GetData("quantity", item.quantity)
+	
+		ix.chat.Send(item.player, "iteminternal", "uses a sterile tool set from their "..item.name..".", false)
+
+		quantity = quantity - 1
+		if (quantity >= 1) then
+			item:SetData("quantity", quantity)
+			return false
+		end
+
+		return true
+	end,
+	OnCanRun = function(item)
+		return (!IsValid(item.entity))
+	end
+}
+
+
+
+function ITEM:GetDescription()
+	if (!self.entity or !IsValid(self.entity)) then
+		local quant = self:GetData("quantity", self.quantity)
+		local str = self.longdesc.."\n \nThere are "..quant.." sterile tool sets stored inside."
+
+		return str
+	else
+		return self.description
+	end
+end
+
+if (CLIENT) then
+	function ITEM:PaintOver(item, w, h)
+		draw.SimpleText(item:GetData("quantity", item.quantity), "DermaDefault", 3, h - 1, color_white, TEXT_ALIGN_LEFT, TEXT_ALIGN_BOTTOM, 1, color_black)
+	end
+end
